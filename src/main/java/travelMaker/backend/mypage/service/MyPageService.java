f6bba875410769709fe6b93d0235278913814098
donysony <doffltm9812@gmail.com>
2023-11-02 12:03:46 +0900

    [setting] 엔티티 추가 및 2차 구현 세팅 TRAV8-29

--- src/main/java/travelMaker/backend/mypage/service/MyPageService.java
+++ src/main/java/travelMaker/backend/mypage/service/MyPageService.java
@@ -3,19 +3,7 @@ package travelMaker.backend.mypage.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import travelMaker.backend.common.error.ErrorCode;
-import travelMaker.backend.common.error.GlobalException;
-import travelMaker.backend.schedule.dto.request.ScheduleRegisterDto;
-import travelMaker.backend.schedule.repository.DateRepository;
-import travelMaker.backend.schedule.repository.ScheduleRepository;
-import travelMaker.backend.tripPlan.model.TripPlan;
-import travelMaker.backend.tripPlan.repository.TripPlanRepository;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-import java.util.Optional;
 
 @Slf4j
 @Service

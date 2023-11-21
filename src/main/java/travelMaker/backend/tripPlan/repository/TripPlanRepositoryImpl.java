81a23c80cde18b5317e44affbca03e157fc517ec
Hyukjin-Ko <1rhgurwls1@naver.com>
2023-11-21 14:59:16 +0900

    [refactor] 검색 isEmpty를 통한 코드수정

--- src/main/java/travelMaker/backend/tripPlan/repository/TripPlanRepositoryImpl.java
+++ src/main/java/travelMaker/backend/tripPlan/repository/TripPlanRepositoryImpl.java
@@ -6,6 +6,7 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.ObjectUtils;
 import travelMaker.backend.tripPlan.dto.request.SearchRequest;
 import travelMaker.backend.tripPlan.dto.response.SearchRegionDto;
 import travelMaker.backend.tripPlan.dto.response.SummaryTripPlan;
@@ -15,6 +16,7 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.springframework.util.ObjectUtils.isEmpty;
 import static travelMaker.backend.schedule.model.QDate.date;
 import static travelMaker.backend.schedule.model.QSchedule.schedule;
 import static travelMaker.backend.tripPlan.model.QTripPlan.tripPlan;
@@ -33,23 +35,14 @@ public class TripPlanRepositoryImpl implements TripPlanRepositoryCustom {
                 .leftJoin(date).on(tripPlan.date.eq(date))
                 .leftJoin(user).on(schedule.user.eq(user))
                 .where(
-                        tripPlan.wishJoin.eq(true)
+                        tripPlan.wishJoin.eq(true),
+                        betweenPerson(searchRequest.getMinPerson(), searchRequest.getMaxPerson()),
+                        targetDate(searchRequest.getTargetStartDate(), searchRequest.getTargetFinishDate()),
+                        targetRegion(searchRequest.getRegion()),
+                        targetGender(searchRequest.getGender()),
+                        targetAgeRange(searchRequest.getAgeRange())
                 );
-        if (searchRequest.getGender() != null) {
-            results = results.where(schedule.user.userGender.eq(searchRequest.getGender()));
-        }
-        if (searchRequest.getAgeRange() != null) {
-            results = results.where(schedule.user.userAgeRange.eq(searchRequest.getAgeRange()));
-        }
-        if (searchRequest.getRegion() != null){
-            results = results.where(tripPlan.region.eq(searchRequest.getRegion()));
-        }
-        if (searchRequest.getTargetStartDate() != null && searchRequest.getTargetFinishDate() != null){
-            results = results.where(targetDate(searchRequest.getTargetStartDate(),searchRequest.getTargetFinishDate()));
-        }
-        if (searchRequest.getMinPerson() != null && searchRequest.getMaxPerson() != null){
-            results = results.where(betweenPerson(searchRequest.getMinPerson(), searchRequest.getMaxPerson()));
-        }
+
         List<TripPlan> searchedresults = results.fetch();
         List<TripPlan> getResults = new ArrayList<>();
         for (TripPlan tripPlan1 : searchedresults) {
@@ -117,7 +110,9 @@ public class TripPlanRepositoryImpl implements TripPlanRepositoryCustom {
                         schedule.scheduleId,
                         date.scheduledDate,
                         tripPlan.arriveTime,
-                        tripPlan.leaveTime
+                        tripPlan.leaveTime,
+                        tripPlan.address,
+                        tripPlan.destinationName
                 )
                 .from(tripPlan)
                 .leftJoin(date).on(tripPlan.date.eq(date))
@@ -125,23 +120,13 @@ public class TripPlanRepositoryImpl implements TripPlanRepositoryCustom {
                 .where(
                         tripPlan.wishJoin.eq(true),
                         tripPlan.destinationX.eq(destinationX),
-                        tripPlan.destinationY.eq(destinationY)
+                        tripPlan.destinationY.eq(destinationY),
+                        betweenPerson(searchRequest.getMinPerson(), searchRequest.getMaxPerson()),
+                        targetDate(searchRequest.getTargetStartDate(), searchRequest.getTargetFinishDate()),
+                        targetRegion(searchRequest.getRegion()),
+                        targetGender(searchRequest.getGender()),
+                        targetAgeRange(searchRequest.getAgeRange())
                 );
-        if (searchRequest.getGender() != null) {
-            query = query.where(schedule.user.userGender.eq(searchRequest.getGender()));
-        }
-        if (searchRequest.getAgeRange() != null) {
-            query = query.where(schedule.user.userAgeRange.eq(searchRequest.getAgeRange()));
-        }
-        if (searchRequest.getRegion() != null){
-            query = query.where(tripPlan.region.eq(searchRequest.getRegion()));
-        }
-        if (searchRequest.getTargetStartDate() != null && searchRequest.getTargetFinishDate() != null){
-            query = query.where(targetDate(searchRequest.getTargetStartDate(),searchRequest.getTargetFinishDate()));
-        }
-        if (searchRequest.getMinPerson() != null && searchRequest.getMaxPerson() != null){
-            query = query.where(betweenPerson(searchRequest.getMinPerson(), searchRequest.getMaxPerson()));
-        }
         List<Tuple> results = query.fetch();
         List<SummaryTripPlan> searchedResults = new ArrayList<>();
         String address = null;
@@ -167,13 +152,22 @@ public class TripPlanRepositoryImpl implements TripPlanRepositoryCustom {
                 .build();
     }
     public BooleanExpression betweenPerson(Integer minPerson, Integer maxPerson){
-        BooleanExpression isMinPerson = tripPlan.wishCnt.goe(minPerson);
-        BooleanExpression isMaxPerson = tripPlan.wishCnt.loe(maxPerson);
+        BooleanExpression isMinPerson = isEmpty(minPerson) ? null : tripPlan.wishCnt.goe(minPerson);
+        BooleanExpression isMaxPerson = isEmpty(maxPerson) ? null : tripPlan.wishCnt.loe(maxPerson);
         return Expressions.allOf(isMinPerson, isMaxPerson);
     }
     public BooleanExpression targetDate(LocalDate targetStartDate, LocalDate targetFinishDate){
-        BooleanExpression isAfterStartDate = tripPlan.date.scheduledDate.goe(targetStartDate);
-        BooleanExpression isBeforeFinishDate = tripPlan.date.scheduledDate.loe(targetFinishDate);
+        BooleanExpression isAfterStartDate = isEmpty(targetStartDate) ? null : tripPlan.date.scheduledDate.goe(targetStartDate);
+        BooleanExpression isBeforeFinishDate = isEmpty(targetFinishDate) ? null : tripPlan.date.scheduledDate.loe(targetFinishDate);
         return Expressions.allOf(isAfterStartDate, isBeforeFinishDate);
+    }
+    private BooleanExpression targetRegion(String region){
+        return isEmpty(region) ? null : tripPlan.region.eq(region);
+    }
+    private BooleanExpression targetGender(String gender){
+        return isEmpty(gender) ? null : schedule.user.userGender.eq(gender);
+    }
+    private BooleanExpression targetAgeRange(String ageRange){
+        return isEmpty(ageRange) ? null : schedule.user.userAgeRange.eq(ageRange);
     }
 }
